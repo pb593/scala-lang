f8c45c184a001ba51e739f4eca6607f6a6e92073
Pavel Berkovich <pberkovich1994@gmail.com>
2016-09-12 16:48:48 -0400

    Lab 8 done.

--- src/labs/lab8/Main.scala
+++ src/labs/lab8/Main.scala
@@ -5,4 +5,34 @@ package labs.lab8
   */
 object Main extends App {
 
+  // Ex. 1
+
+  def op(opCode: String): (Int, Int) => Int =
+    opCode match {
+      case "add" => _ + _
+      case "sub" => _ - _
+      case "div" => _ / _
+      case "mul" => _ * _
+    }
+
+  val add = op("add")
+  println(add(5, 11))
+
+  // Ex. 2
+
+  class Client {}
+  class JuniorClient extends Client {}
+  class RegularClient extends Client {}
+  class SeniorClient extends Client {}
+
+  def allowedToBorrow(c: Client) =
+    c match {
+      case _: JuniorClient => 5
+      case _: RegularClient => 10
+      case _: SeniorClient => 15
+    }
+
+
+
+
 }
